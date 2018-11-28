3506a21e4263c7c0c757a3e2b562807fc71eb3a7
kittisitthichai <kittisitthichai30084@gmail.com>
2018-11-28 15:25:29 +0700

    First Initialize by Kitti

---
+++ pages/url.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('', views.about, name='about'),
+    path('', views.listing, name='listing'),
+]
